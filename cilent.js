ed38940b16fc66a87a3a115c01cfd1fa7425533b
K-Sravan-Kumar <sravan.kumar1691@gmail.com>
2018-02-04 15:40:37 +0530

    first web basic animtations

---
+++ cilent.js
@@ -0,0 +1,11 @@
+function retry() {
+	document.getElementById("textchange").innerHTML = "please try again";
+}
+
+function login() {
+	if 
+	{
+		document.getElementById("username").define == "sravan";
+		document.getElementById("password").define == "12345789";
+	}
+}
